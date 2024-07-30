fa80ae8574545aaab2068d89fe78c85130e546b3
SeungJu Mun <136792663+NUMCHCOMCH@users.noreply.github.com>
2024-07-30 10:34:29 +0900

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -14,7 +14,6 @@ st.set_page_config(
 )
 
 api_key = st.secrets['TOKEN'] 
-st.write(api_key)
 
 def upload_to_github(token, repo, path, content):
     url = f"https://api.github.com/repos/{repo}/contents/{path}"
